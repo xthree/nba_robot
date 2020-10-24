a7aa074c6acb81d3d8d18b15185ad023f7394a2c
xthree <rmoore08@gmail.com>
2020-10-24 14:46:02 -0600

    separated out not running correctly. Main doesn't know what basketball game is yet.  incorrect load order?

---
+++ src/scripts/main.ts
@@ -0,0 +1,37 @@
+
+namespace ESPNSS {
+  var jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  window = new JSDOM();
+  document = (new JSDOM('')).window;
+  global.document = document;
+  //@ts-ignore
+  $ = jQuery = require('jquery')(new jsdom.JSDOM().window);
+
+  const rp = require('request-promise');
+  //let jsonFile = require('jsonfile');
+  const fs = require('fs');
+
+
+
+  // Scheduler
+  // const scheduler = require('node-schedule');
+  // rp("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard").then((e) => {
+  //   var data = JSON.parse(e);
+  //   for (let event of data.events) {
+  //     let gameStartTime = new Date(event.date);
+  //     gameStartTime.setMinutes(gameStartTime.getMinutes() - 15);
+
+  //     console.log(`${event.name} scheduled for ${gameStartTime}. ${event.id}`);
+  //     scheduleGame(gameStartTime, event.id);
+  //   }
+  // });
+
+  // function scheduleGame(pStartTime, pGameId) {
+  //   scheduler.scheduleJob(pStartTime, function (y) {
+  //     new BasketballGame(pGameId, fs);
+  //   });
+  // }
+
+  new BasketballGame("401248438", fs);
+}
---
+++ src/scripts/team.ts
@@ -0,0 +1,49 @@
+namespace ESPNSS {
+
+    export class Team {
+        private $wrapper: JQuery;
+        private isHomeTeam: boolean;
+        private UID: string;
+        private location: string;
+        public name: string;
+        private nameAbbreviation: string;
+        public score: number;
+        private record: string;
+        public players: Player[];
+
+        constructor(pWrapper: JQuery, pUID: string, pLocation: string, pTeamName: string, pTeamNameAbbreviation: string, pIsHomeTeam: boolean) {
+            this.$wrapper = $(pWrapper);
+            this.isHomeTeam = pIsHomeTeam;
+            this.UID = pUID;
+            this.location = pLocation;
+            this.name = pTeamName;
+            this.nameAbbreviation = pTeamNameAbbreviation;
+        }
+
+        public updateTeamData(pScore: number, pGeneratePlayerData: boolean = false) {
+            this.score = pScore;
+            if (pGeneratePlayerData) {
+                this.generatePlayerData();
+            }
+        }
+
+        public updateContainer($pNewWrapper: JQuery) {
+            this.$wrapper = $pNewWrapper;
+        }
+
+        public generatePlayerData() {
+            let $players = this.$wrapper.find("tbody tr").not('.highlight');
+            this.players = [];
+
+            // Counts score for accuracy check
+            let scoreCount = 0;
+
+            $players.each((i, row) => {
+                this.players.push(new Player($(row)));
+                scoreCount += isNaN(this.players[i].points) ? 0 : this.players[i].points;
+            });
+
+            scoreCount == this.score ? "" : console.warn(`scores do not match. Expected ${this.score}. Got ${scoreCount}`)
+        }
+    }
+}
