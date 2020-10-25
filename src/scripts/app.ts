901e81a27e662685ca8efec6a7bb056bc63dfda6
xthree <rmoore08@gmail.com>
2020-10-24 23:12:11 -0600

    Runs now - added dependencies to top of every file

---
+++ src/scripts/app.ts
@@ -0,0 +1,26 @@
+import { BasketballGame } from "./basketballGame"
+
+let jsdom = require("jsdom");
+let $ = require('jquery')(new jsdom.JSDOM().window);
+
+const rp = require('request-promise');
+const fs = require('fs');
+
+// Scheduler
+function scheduleGame(pStartTime, pGameId) {
+  scheduler.scheduleJob(pStartTime, function (y) {
+    new BasketballGame(pGameId, fs);
+  });
+}
+
+const scheduler = require('node-schedule');
+rp("http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard").then((e) => {
+  var data = JSON.parse(e);
+  for (let event of data.events) {
+    let gameStartTime = new Date(event.date);
+    gameStartTime.setMinutes(gameStartTime.getMinutes() - 15);
+
+    console.log(`${event.name} scheduled for ${gameStartTime}. ${event.id}`);
+    scheduleGame(gameStartTime, event.id);
+  }
+});
--- src/scripts/team.ts
+++ src/scripts/team.ts
@@ -1,49 +1,51 @@
-namespace ESPNSS {
+import { Player } from "./player"
 
-    export class Team {
-        private $wrapper: JQuery;
-        private isHomeTeam: boolean;
-        private UID: string;
-        private location: string;
-        public name: string;
-        private nameAbbreviation: string;
-        public score: number;
-        private record: string;
-        public players: Player[];
+let jsdom = require("jsdom");
+let $ = require('jquery')(new jsdom.JSDOM().window);
 
-        constructor(pWrapper: JQuery, pUID: string, pLocation: string, pTeamName: string, pTeamNameAbbreviation: string, pIsHomeTeam: boolean) {
-            this.$wrapper = $(pWrapper);
-            this.isHomeTeam = pIsHomeTeam;
-            this.UID = pUID;
-            this.location = pLocation;
-            this.name = pTeamName;
-            this.nameAbbreviation = pTeamNameAbbreviation;
+export class Team {
+    private $wrapper: JQuery;
+    private isHomeTeam: boolean;
+    private UID: string;
+    private location: string;
+    public name: string;
+    private nameAbbreviation: string;
+    public score: number;
+    private record: string;
+    public players: Player[];
+
+    constructor(pWrapper: JQuery, pUID: string, pLocation: string, pTeamName: string, pTeamNameAbbreviation: string, pIsHomeTeam: boolean) {
+        this.$wrapper = $(pWrapper);
+        this.isHomeTeam = pIsHomeTeam;
+        this.UID = pUID;
+        this.location = pLocation;
+        this.name = pTeamName;
+        this.nameAbbreviation = pTeamNameAbbreviation;
+    }
+
+    public updateTeamData(pScore: number, pGeneratePlayerData: boolean = false) {
+        this.score = pScore;
+        if (pGeneratePlayerData) {
+            this.generatePlayerData();
         }
+    }
 
-        public updateTeamData(pScore: number, pGeneratePlayerData: boolean = false) {
-            this.score = pScore;
-            if (pGeneratePlayerData) {
-                this.generatePlayerData();
-            }
-        }
+    public updateContainer($pNewWrapper: JQuery) {
+        this.$wrapper = $pNewWrapper;
+    }
 
-        public updateContainer($pNewWrapper: JQuery) {
-            this.$wrapper = $pNewWrapper;
-        }
+    public generatePlayerData() {
+        let $players = this.$wrapper.find("tbody tr").not('.highlight');
+        this.players = [];
 
-        public generatePlayerData() {
-            let $players = this.$wrapper.find("tbody tr").not('.highlight');
-            this.players = [];
+        // Counts score for accuracy check
+        let scoreCount = 0;
 
-            // Counts score for accuracy check
-            let scoreCount = 0;
+        $players.each((i, row) => {
+            this.players.push(new Player($(row)));
+            scoreCount += isNaN(this.players[i].points) ? 0 : this.players[i].points;
+        });
 
-            $players.each((i, row) => {
-                this.players.push(new Player($(row)));
-                scoreCount += isNaN(this.players[i].points) ? 0 : this.players[i].points;
-            });
-
-            scoreCount == this.score ? "" : console.warn(`scores do not match. Expected ${this.score}. Got ${scoreCount}`)
-        }
+        scoreCount == this.score ? "" : console.warn(`scores do not match. Expected ${this.score}. Got ${scoreCount}`)
     }
 }
