587ad52cfa0bd11d668d1b96661e73f041cdf942
xthree <rmoore08@gmail.com>
2021-01-09 19:16:11 -0700

    added gameCast url to ESPN

--- src/scripts/helpers/ESPN.ts
+++ src/scripts/helpers/ESPN.ts
@@ -1,7 +1,9 @@
 export class ESPN {
     public static readonly schedule = `https://www.espn.com/nba/schedule/_/date/`; // ADD DATE YYYYMMDD
     public static readonly boxScore = `https://www.espn.com/nba/boxscore?gameId=`; //ADD GAMEID
-    public static readonly hiddenAPI = `http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard`; // Add nothing
+    public static readonly gameCast = `https://www.espn.com/nba/game?gameId=`; //ADD GAMEID
+    
+    public static readonly hiddenAPI = `http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard`; // Add nothing, see 
 }
 
 // {
