6df4dd2f7052a05db6dd480bc9bfe475363cd669
Ryan Moore <ryan.moore@smart-monkeys.com>
2022-11-04 18:18:25 -0600

    beta keys with rread and write access

--- src/scripts/config/keys.ts
+++ src/scripts/config/keys.ts
@@ -6,13 +6,17 @@ const keys = {
     twitter_user_secret: "gsWYCH4fA7vz4k7Da0A7mhqUlag1pbDy42TrktvkHUdYI",
   },
   test: {
-    twitter_application_consumer_key: "r912KOTgGKrAJ8hDEx7VLec5s", // API Key
-    twitter_application_secret: "2EI3HcucNUaxiEptaoMAmtW2HWbcw3xkE9cXwAQEu6qtpsBSbc", // API secret
-    twitter_user_access_token: "1588660592835170305-Rlbtc776Ay3pD6EWrHwy7Rylfh7exe", // Access Token
-    twitter_user_secret: "W44msViGPefhKSrZiDRD73aLcAvTu9ClATBpFFS9xDEF7", // Access Token Secret
+    twitter_application_consumer_key: "yTDXKUT72KUNtfVQLuh6GXT7K", // API Key
+    twitter_application_secret: "L2g4zevCphqpzTJaEubVwuXNOJ4UArnOskT51LLa7jWOi6gBpr", // API secret
+    twitter_user_access_token: "1588660592835170305-XCM8SACHssOtiGmTCTRrUndqTyP9Uo", // Access Token
+    twitter_user_secret: "CQzkzhWyEmrn2TMS8gfNdaaYyQbwh8MlWu35baSpEQ3E0", // Access Token Secret
   },
 };
 
 export default keys;
 
 //test bearerToken AAAAAAAAAAAAAAAAAAAAADLNiwEAAAAAlbz4IrwKYb1sK9jKRcb%2Bhry7PwQ%3DbTH6AwOqRlxTaScQMX0cYWONBZUj4yCi9JDRWFbOGl0atCOHTs
+
+
+//dzZMQ2FyRDNiaFY0SXZabVlyM1Q6MTpjaQ
+//dzZMQ2FyRDNiaFY0SXZabVlyM1Q6MTpjaQ
